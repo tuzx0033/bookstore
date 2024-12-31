ad785bcb5e90e804bb99a4bcb511cbfaad09af81
Tú Nguyễn <tunguyen@Tus-MacBook-Pro.local>
2024-12-31 13:33:07 +0700

    new

---
+++ Models/Book.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.Models
+{
+    public class Book
+    {
+        public int id { get; set; }
+
+        [Required]
+        [Display(Name = "Name")]
+        public string bname { get; set; }
+
+        public string author { get; set; }
+
+        public float? price { get; set; }
+
+        [Required]
+        public DateTime? ReleaseDate { get; set; }
+
+        public DateTime? DateAdded { get; set; }
+
+        public Genre Genre { get; set; }
+
+        [Display(Name = "Genre")]
+        public int GenreId { get; set; }
+
+        [Display(Name = "Number in Stock")]
+        [Range(1,20)]
+        public int AvailCopies { get; set; }
+
+    }
+}
